2fa0ad3d7b5844e1cd733a4dd6a36966f91d62a8
David Latorre <dvdn10071856@gmail.com>
2018-10-21 21:53:11 -0500

    Add more functions

--- main.py
+++ main.py
@@ -4,8 +4,53 @@
 
 clients = 'Pablo, Ricardo, '
 
+def create_client(client_name):
+    global clients
 
-if __name__ == '__main__':
-    clients += 'David'
+   # input(str("Añada el nombre del cliente"))
+    clients += client_name
+    _add_comma()
+
+
+def list_clients():
+    global clients
+
+    print ('The clients list is: ')
     print (clients)
 
+
+def _add_comma():
+    global clients
+
+    clients += ','
+
+def _space_line():
+    print ('*' * 50)
+
+def _print_welcome():
+    _space_line()
+    print('Welcome to VentasLab')
+    _space_line()
+    print ('what would you want to do today?')
+    print ('[C]reate client')
+    print ('[D]elete client')
+    print ('[E]xit')
+
+if __name__ == '__main__':
+
+    _print_welcome()
+
+    command = input()
+
+    if command == 'C':
+        client_name = input('What is the client name?: ')
+        create_client(client_name)
+        list_clients()
+    elif command == 'D':
+        pass
+    elif command == 'E':
+        _space_line
+        print('Thanks for using VentasLab services')
+        exit()
+    else:
+        print ('Invalid command')
