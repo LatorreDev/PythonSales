80899d433bc1811614dbdd599f3534342c986822
David Latorre <dvdn10071856@gmail.com>
2018-10-22 21:35:57 -0500

    Add logic to update option

--- main.py
+++ main.py
@@ -20,6 +20,13 @@ def list_clients():
     print ('The clients list is: ')
     print (clients)
 
+def update_client(client_name, updated_client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', updated_client_name)
+    else:
+        print ('Client is not in clients list')
 
 def _add_comma():
     global clients
@@ -40,6 +47,9 @@ def _print_welcome():
     print ('[D]elete client')
     print ('[E]xit')
 
+def _get_client_name():
+    return input('What is the client name?: ')
+
 if __name__ == '__main__':
 
     _print_welcome()
@@ -48,15 +58,21 @@ if __name__ == '__main__':
     command = command.upper()
 
     if command == 'C':
-        client_name = input('What is the client name?: ')
+        client_name = _get_client_name()
         create_client(client_name)
         list_clients()
+
     elif command == 'R':
         pass
+
     elif command == 'U':
-        pass
+        client_name = _get_client_name()
+        updated_client_name = input('Whats is the client name?: ')
+        update_client(client_name)
+
     elif command == 'D':
         pass
+
     elif command == 'E':
         _space_line
         print('Thanks for using VentasLab services')
